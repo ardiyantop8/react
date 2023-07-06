983d7213fa2131212e139ae79ab33e3b36b75592
Ardiyanto <ardiyantop8@gmail.com>
2023-07-06 16:27:19 +0700

    Menambahkan library ant design versi 5 dan library tailwind css

---
+++ porto/src/Template/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      {/* Konten halaman lainnya */}
+      <footer className="mt-auto">Footer Content</footer>
+    </div>
+  );
+};
+
+export default Footer;
